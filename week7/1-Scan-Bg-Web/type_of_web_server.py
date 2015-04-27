0d47ee93fb45df9aef4f3fa0b83fbd02726d806a
anedelcheva <aneta.nedelcheva@gmail.com>
2015-04-27 22:10:24 +0300

    Adding class FindLinksFromAnHtmlOfAPage

---
+++ week7/1-Scan-Bg-Web/type_of_web_server.py
@@ -0,0 +1,36 @@
+import requests
+from bs4 import BeautifulSoup
+
+class FindLinksFromAnHtmlOfAPage:
+
+# the instructor receives a url which is a web page
+    def __init__(self, url):
+        r = requests.get(url)
+        #r.text contains all the html document in a string format
+        #in self.html we have the html file of the webpage. BeautifulSoup
+        #converts the string r.text to an html document
+        self.html = BeautifulSoup(r.text)
+
+    def put_all_hrefs_to_list(self):
+        hrefs_list = []
+        #getting all hrefs from a valid html document
+        for link in self.html.find_all('a'):
+            hrefs_list.append(link.get('href'))
+        #filtering those hrefs which are conform
+        #with the pattern link.php?id=64722
+        link_php = 'link.php?id='
+        hrefs_list_filtered = []
+        for href in hrefs_list:
+            if link_php in str(href):
+                hrefs_list_filtered.append(href)
+        return hrefs_list_filtered
+
+
+'''r = requests.get('http://register.start.bg/#b_6118').text
+print (r)'''
+
+links = FindLinksFromAnHtmlOfAPage('http://register.start.bg/#b_6118')
+print (links.put_all_hrefs_to_list())
+
+#print (r.headers['Server'])
+#r2 = requests.get('https://gsm.bg')
