d1977ef17325d19e0c87e01983565061308e3dfa
anedelcheva <aneta.nedelcheva@gmail.com>
2015-04-28 22:05:59 +0300

    Adding more methods

--- week7/1-Scan-Bg-Web/scan_bg_web.py
+++ week7/1-Scan-Bg-Web/scan_bg_web.py
@@ -1,26 +1,24 @@
 import requests
 from bs4 import BeautifulSoup
+from histogram import Histogram
 
 
 class ScanBgWeb:
 
-# the instructor receives a url which is a web page
+    my_headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'}
+
     def __init__(self, url):
+        self.url = url
         r = requests.get(url)
-        #r.text contains all the html document in a string format
-        #in self.html we have the html file of the webpage. BeautifulSoup
-        #converts the string r.text to an html document
         self.html = BeautifulSoup(r.text)
+        self.histogram = Histogram()
 
     def put_all_hrefs_to_list(self):
         hrefs_list = []
-        #getting all hrefs from a valid html document
         for link in self.html.find_all('a'):
             hrefs_list.append(link.get('href'))
         return hrefs_list
 
-        #filtering those hrefs which conform
-        #with the pattern link.php?id=64722
     def filter_hrefs(self):
         hrefs_list = self.put_all_hrefs_to_list()
         link_php = 'link.php?id='
@@ -30,12 +28,34 @@ class ScanBgWeb:
                 hrefs_list_filtered.append(href)
         return hrefs_list_filtered
 
+    def get_servers(self):
+        hrefs_list = self.filter_hrefs()
+        bg_servers = []
+        for href in hrefs_list:
+            try:
+                response = requests.head(self.url + '/' + href, allow_redirects=True, timeout=3, headers=my_headers)
+                bg_servers.append(response.headers['server'])
+                print (response.headers['server'])
+            except:
+                pass
+        return bg_servers
 
-'''r = requests.get('http://register.start.bg/#b_6118').text
-print (r)'''
+    def servers_to_file(self):
+        bg_servers = self.get_servers()
+        with open('servers_bg.txt', 'w') as f:
+            for server in bg_servers:
+                f.write(server + '\n')
 
-links = ScanBgWeb('http://register.start.bg/#b_6118')
-print (len(links.filter_hrefs()))
-
-#print (r.headers['Server'])
-#r2 = requests.get('https://gsm.bg')
+    def get_statistics(self):
+        with open('servers_bg.txt', 'r') as f:
+            servers = f.read()
+            server_list = servers.split('\n')
+            for server in server_list:
+                if 'Apache' in server:
+                    self.histogram.add('Apache')
+                elif 'nginx' in server:
+                    self.histogram.add('nginx')
+                elif 'IIS' in server:
+                    self.histogram.add('IIS')
+                elif 'lighttpd' in server:
+                    self.histogram.add('lighttpd')
