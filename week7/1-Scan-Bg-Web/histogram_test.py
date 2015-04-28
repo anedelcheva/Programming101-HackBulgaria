b9276142edb8301f79a109073b3512aa407734c3
anedelcheva <aneta.nedelcheva@gmail.com>
2015-04-28 21:02:33 +0300

    Adding some little modifications

--- week7/1-Scan-Bg-Web/histogram_test.py
+++ week7/1-Scan-Bg-Web/histogram_test.py
@@ -16,16 +16,17 @@ class HistogramTest(unittest.TestCase):
         self.assertTrue(isinstance(self.h, Histogram))
 
     def test_add(self):
-        web_servers = {'Apache': 2, 'nginx': 2, 'IIS': 1}
+        web_servers = {'Apache': 2, 'nginx': 2, 'IIS': 1, 'lighttpd': 0}
         self.assertEqual(self.h.web_servers, web_servers)
 
     def test_count(self):
         self.assertEqual(self.h.count('Apache'), 2)
         self.assertEqual(self.h.count('nginx'), 2)
         self.assertEqual(self.h.count('IIS'), 1)
+        self.assertEqual(self.h.count('lighttpd'), 0)
 
     def test_get_dict(self):
-        web_servers = {'Apache': 2, 'nginx': 2, 'IIS': 1}
+        web_servers = {'Apache': 2, 'nginx': 2, 'IIS': 1, 'lighttpd': 0}
         self.assertEqual(self.h.web_servers, web_servers)
 
 if __name__ == '__main__':
