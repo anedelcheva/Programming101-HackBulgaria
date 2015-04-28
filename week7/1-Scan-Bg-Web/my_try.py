d67954ba325731cd48b47cab68bfe17b80a127d5
anedelcheva <aneta.nedelcheva@gmail.com>
2015-04-28 20:54:09 +0300

    Adding more methods

--- week7/1-Scan-Bg-Web/my_try.py
+++ week7/1-Scan-Bg-Web/my_try.py
@@ -6,6 +6,8 @@ from histogram import Histogram
 
 class ScanBgWeb:
 
+    my_headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'}
+
     def __init__(self, url):
         self.url = url
         r = requests.get(url)
@@ -32,9 +34,9 @@ class ScanBgWeb:
         bg_servers = []
         for href in hrefs_list:
             try:
-                response = requests.get(self.url + '/' + href, timeout=3)
+                response = requests.head(self.url + '/' + href, allow_redirects=True, timeout=3, headers=my_headers)
                 bg_servers.append(response.headers['server'])
-                #print (response.headers['server'])
+                print (response.headers['server'])
             except:
                 pass
         return bg_servers
@@ -43,11 +45,27 @@ class ScanBgWeb:
         bg_servers = self.get_servers()
         with open('servers_bg.txt', 'w') as f:
             for server in bg_servers:
-                #print (server)
-                f.write(server + '/n')
+                f.write(server + '\n')
+
+    def get_statistics(self):
+        with open('servers_bg.txt', 'r') as f:
+            servers = f.read()
+            server_list = servers.split('\n')
+            #print(server_list)
+            for server in server_list:
+                if 'Apache' in server:
+                    self.histogram.add('Apache')
+                elif 'nginx' in server:
+                    self.histogram.add('nginx')
+                elif 'IIS' in server:
+                    self.histogram.add('IIS')
+                elif 'lighttpd' in server:
+                    self.histogram.add('lighttpd')
 
 web = ScanBgWeb('http://register.start.bg')
-r = requests.get(web.url).headers['server']
-print (r)
+r = requests.head(web.url).headers['server']
+#print (r)
 #print (web.get_servers())
-web.servers_to_file()
+#web.servers_to_file()
+web.get_statistics()
+print (web.histogram.get_dict())
