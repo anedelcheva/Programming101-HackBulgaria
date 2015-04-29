4ba930dfa7712ba789ddc3ca2d62c09b3bd1419f
anedelcheva <aneta.nedelcheva@gmail.com>
2015-04-29 22:05:42 +0300

    Optimizing create_company.py

--- week7/2-SQL-Starter/create_company.py
+++ week7/2-SQL-Starter/create_company.py
@@ -5,8 +5,8 @@ create_table_query = '''
 CREATE TABLE IF NOT EXISTS employees(
 id INTEGER PRIMARY KEY,
 name TEXT,
-monthly_salary TEXT,
-yearly_bonus TEXT,
+monthly_salary INTEGER,
+yearly_bonus INTEGER,
 position TEXT
 )'''
 
@@ -18,35 +18,36 @@ name1 = 'Ivan Ivanov'
 salary1 = 5000
 bonus1 = 10000
 position1 = 'Software Developer'
-c.execute('''INSERT INTO employees(name, monthly_salary, yearly_bonus, position)
-            VALUES(?, ?, ?, ?)''', (name1, salary1, bonus1, position1))
 
 name2 = 'Rado Rado'
 salary2 = 500
 bonus2 = 0
 position2 = 'Technical Support Intern'
-c.execute('''INSERT INTO employees(name, monthly_salary, yearly_bonus, position)
-            VALUES(?, ?, ?, ?)''', (name2, salary2, bonus2, position2))
 
 name3 = 'Ivo Ivo'
 salary3 = 10000
 bonus3 = 100000
 position3 = 'CEO'
-c.execute('''INSERT INTO employees(name, monthly_salary, yearly_bonus, position)
-            VALUES(?, ?, ?, ?)''', (name3, salary3, bonus3, position3))
 
 name4 = 'Petar Petrov'
 salary4 = 3000
 bonus4 = 1000
 position4 = 'Marketing Manager'
-c.execute('''INSERT INTO employees(name, monthly_salary, yearly_bonus, position)
-            VALUES(?, ?, ?, ?)''', (name4, salary4, bonus4, position4))
 
 name5 = 'Maria Georgieva'
 salary5 = 8000
 bonus5 = 10000
 position5 = 'COO'
-c.execute('''INSERT INTO employees(name, monthly_salary, yearly_bonus, position)
-            VALUES(?, ?, ?, ?)''', (name5, salary5, bonus5, position5))
+
+workers = [
+(name1, salary1, bonus1, position1),
+(name2, salary2, bonus2, position2),
+(name3, salary3, bonus3, position3),
+(name4, salary4, bonus4, position4),
+(name5, salary5, bonus5, position5)
+]
+
+c.executemany('''INSERT INTO employees(name, monthly_salary, yearly_bonus, position)
+            VALUES(?, ?, ?, ?)''', workers)
 
 db.commit()
