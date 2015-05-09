b19c2ba3bba1d2e6f436ff6d27af39fed66ab7f6
anedelcheva <aneta.nedelcheva@gmail.com>
2015-05-09 12:36:11 +0300

    Adding some functions

--- week8/3-Cinema-Reservation-System/magic_reservation_system.py
+++ week8/3-Cinema-Reservation-System/magic_reservation_system.py
@@ -49,6 +49,8 @@ def projections_number_reservations():
 
 
 def spots_number():
+    print ("Total number of spots available for each projection: ")
+
     cursor.execute('''
     SELECT Projections.id, Movies.name, Projections.type,
     Projections.date, Projections.time, COUNT(Projections.id) AS number
@@ -57,6 +59,7 @@ def spots_number():
     JOIN Movies
     ON Projections.id = Reservations.projection_id AND Projections.movie_id = Movies.id
     GROUP BY Projections.id''')
+
     for proj in cursor.fetchall():
         spots = SPOTS - proj['number']
         id = proj['id']
@@ -65,8 +68,10 @@ def spots_number():
         date = proj['date']
         time = proj['time']
         print ('[{}] - {} ({}) {} {}--->Spots: {}'.format(id, name, type, date, time, spots))
+
     all_projections = projections()
     projections_reserved = projections_number_reservations()
+
     for proj in all_projections:
         if proj not in projections_reserved:
             projection = choose_projection(proj)
@@ -90,23 +95,25 @@ def movie_projections(movie_id):
     return cursor.fetchall()
 
 
-def show_movie_projections(movie_id, movie_date):
+def show_movie_projections_with_date(movie_id, movie_date):
     movies = movie_projections(movie_id)
-    if movie_date == '':
-        print ("Projections for movie '" + movies[0]['name'] + "':")
-        for movie in movies:
-            id = movie['id']
-            date = movie['date']
-            time = movie['time']
-            type = movie['type']
-            print ('[{}] - {} {} ({})'.format(id, date, time, type))
-    else:
-        print ("Projections for movie '" + movies[0]['name'] + "' on date " + movie_date)
-        for movie in movies:
-            if movie['date'] == movie_date:
-                print ('[{}] - {} ({})'.format(movie['id'], movie['time'], movie['type']))
+    print ("Projections for movie '" + movies[0]['name'] + "' on date " + movie_date + ':')
+    for movie in movies:
+        if movie['date'] == movie_date:
+            print ('[{}] - {} ({})'.format(movie['id'], movie['time'], movie['type']))
+    spots_number()
 
-    print ("Total number of spots available for each projection: ")
+
+def show_movie_projections_without_date(movie_id):
+    movies = movie_projections(movie_id)
+    print ("Projections for movie '" + movies[0]['name'] + "':")
+    for movie in movies:
+        id = movie['id']
+        date = movie['date']
+        time = movie['time']
+        type = movie['type']
+        print ('[{}] - {} {} ({})'.format(id, date, time, type))
+
     spots_number()
 
 
@@ -120,19 +127,23 @@ def read_command():
         command = parse_command(command)
         if command[0] == 'show_movies':
             show_movies()
-        # elif command[0] == 'show_movie_projections':
-        #     print (command)
-        #     # show_movie_projections(int(command[1]), command[2])
+        elif command[0] == 'show_movie_projections':
+            if len(command) == 2:
+                show_movie_projections_without_date(int(command[1]))
+            elif len(command) == 3:
+                show_movie_projections_with_date(int(command[1]), command[2])
+            elif len(command) == 1:
+                print('No given id!')
 
 
 def main():
     read_command()
 
 
-# if __name__ == '__main__':
-#     main()
+if __name__ == '__main__':
+    main()
 
 
-show_movie_projections(1, '2014-04-01')
+# show_movie_projections_without_date(1)
 # spots_number()
 # print (choose_projection(4))
